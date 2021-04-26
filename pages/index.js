048e06f865d15bc283535a8f02ed587b86a56642
Mohit Sojitra <mohitsojitra0@gmail.com>
2021-04-26 22:44:04 +0530

    theming done + next js project setup done

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,9 @@ import { Typography } from "@material-ui/core";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <Typography variant={"h1"}>Hello World</Typography>;
+  return (
+    <Typography variant={"h1"} color={"primary"}>
+      Hello World
+    </Typography>
+  );
 }
